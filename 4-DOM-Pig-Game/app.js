03d1466d09dff128e02d3e4ec4c95d40e1e1d894
cliveharris <37327320+cliveharris@users.noreply.github.com>
2018-04-30 08:30:28 +0100

    Added roll dice event listener

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -7,21 +7,30 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
-let scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random()*6) + 1;
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 
 const x = document.querySelector('#score-' + activePlayer).textContent;
 console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
 
- 
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // 1. random umber
+    let dice = Math.floor(Math.random() * 6) + 1;
+    // 2. display result
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    // 3. update round score if rolled number was not 1.
+});
