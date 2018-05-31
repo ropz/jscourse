b3f94e337087358b9fd045f2ebe7a85ddee62159
Clive Harris <clive.harris@gmail.com>
2018-05-31 21:09:08 +0100

    Lecture 132

--- 9-forkify-starter/src/js/index.js
+++ 9-forkify-starter/src/js/index.js
@@ -1,5 +1,32 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+const state = {
+    // global state of app
+    // Search object - query, results
+    // Current recipe object
+    // Shopping list object
+    // Liked recipes
+};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+        // 3) Prepare for UI results
+        searchView.clearInput();
+        searchView.clearResults();
+        // 4) Search for recipes
+        await state.search.getResults();
+        // 5) render results on UI
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener('submit', e=> {
+    e.preventDefault();
+    controlSearch();
+});
