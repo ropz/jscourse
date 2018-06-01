6b08d4ef802ee448941a944dbf7fa3f38f3e86c1
cliveharris <37327320+cliveharris@users.noreply.github.com>
2018-06-01 09:18:34 +0100

    pagination complete

--- 9-forkify-starter/src/js/index.js
+++ 9-forkify-starter/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 const state = {
     // global state of app
@@ -19,9 +19,11 @@ const controlSearch = async () => {
         // 3) Prepare for UI results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
         // 4) Search for recipes
         await state.search.getResults();
         // 5) render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 };
@@ -30,3 +32,12 @@ elements.searchForm.addEventListener('submit', e=> {
     e.preventDefault();
     controlSearch();
 });
+
+elements.searchResPages.addEventListener('click', e=> {
+ const btn = e.target.closest('.btn-inline');
+ if (btn) {
+  const goToPage = parseInt(btn.dataset.goto,10);
+  searchView.clearResults();
+  searchView.renderResults(state.search.result, goToPage);
+ }
+})
