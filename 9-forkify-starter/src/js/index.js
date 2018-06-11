ea46c2c175ed5e7800338a19a1ff40b3199dee09
cliveharris <37327320+cliveharris@users.noreply.github.com>
2018-06-11 08:03:45 +0100

    List

--- 9-forkify-starter/src/js/index.js
+++ 9-forkify-starter/src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import {
     elements,
     renderLoader,
@@ -16,6 +17,8 @@ const state = {
     // Shopping list object
     // Liked recipes
 };
+// TESTING
+window.state = state;
 
 const controlSearch = async () => {
     // 1) Get query from view
@@ -92,6 +95,32 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+// LIST CONTROLLER
+const controlList = () => {
+    // create a new list if there is not one yet
+    if (!state.list) state.list = new List();
+    // add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+}
+
+// Handle deleetes
+elements.shopping.addEventListener('click', e=>{
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // delete from state
+        state.list.deleteItem(id);
+        // delete from  UI
+        listView.deleteItem(id);
+    } else if (e.target.matches('.shopping__count--value, ')) {
+        // handle cound update
+        const val = parseFloat(e.target.value);
+        state.list.updateCount(id, val);
+    }
+})
+
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e=>{
     console.log('event');
@@ -105,7 +134,9 @@ elements.recipe.addEventListener('click', e=>{
     if (e.target.matches('.btn-increase, .btn-increase *')) {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
-    } // Asterisk means any child
+    } else if (e.target.matches('.btn__recipe--add, .btn__recipe--add *')) {
+        controlList();
+    }
     console.log(state.recipe);
 })
 
